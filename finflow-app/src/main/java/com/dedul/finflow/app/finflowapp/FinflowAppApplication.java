282ece2c6e591c44c66a1f3284594d9a15d118f8
Alexey <dedyul@gmail.com>
2026-06-06 18:03:49 +0200

    chore: project sceleton

---
+++ finflow-app/src/main/java/com/dedul/finflow/app/finflowapp/FinflowAppApplication.java
@@ -0,0 +1,13 @@
+package com.dedul.finflow.app.finflowapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinflowAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinflowAppApplication.class, args);
+  }
+
+}
